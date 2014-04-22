2e28c336d4e89ecc0c20b5e78a022ce8d14a6f5c
Catalyse <spam@catalysetech.com>
2014-04-21 21:02:03 -0600

    Update Setup.py

--- Setup.py
+++ Setup.py
@@ -2,7 +2,7 @@ import sys
 from cx_Freeze import setup, Executable
 
 setup(
-    name = "CubeBlocks Modifier",
-    version = "0.1",
-    description = "Modifys the Time Frames of your CubeBlocks File",
+    name = "CubeBlocks Modifier - v0.5b by Catalyse",
+    version = "0.5b",
+    description = "Modifies the Time Frames of your CubeBlocks File",
     executables = [Executable("X:\workspace\Cubeblocks\src\Main.py", base = "Console")])
